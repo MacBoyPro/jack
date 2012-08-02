8776b3797c75742d3e6c902400f66ab1eb2a80cc
Veselin Todorov <veselin@veselin.bg>
2012-08-02 10:46:26 +0300

    Initial stub prototype

---
+++ test/fixtures/project.js
@@ -0,0 +1,20 @@
+/**
+ * Project fixture.
+ */
+function Project() {
+};
+
+/**
+ * Simple class method.
+ *
+ * @returns {Integer}
+ * @api public
+ */
+Project.id = function() {
+  return 1;
+};
+
+/**
+ * Expose `Project`.
+ */
+module.exports = Project;
---
+++ test/jack.test.js
@@ -0,0 +1,37 @@
+/**
+ * Assertions.
+ */
+var chai = require('chai');
+var should = chai.should();
+
+/**
+ * Fixtures.
+ */
+var Project = require('./fixtures/project');
+
+/**
+ * Subject.
+ *
+ * @type {Object}
+ */
+var jack = require('../');
+
+describe('jack', function() {
+  it('can stub class methods', function() {
+    Project.stubs('id').returns(2);
+    Project.id().should.eql(2);
+    Project.resetAll();
+  });
+
+  it('can stub class methods based on conditions', function() {
+    Project.stubs('id').with(3).returns(3);
+    Project.stubs('id').with(4).returns(4);
+
+    Project.id(3).should.eql(3);
+    Project.id(4).should.eql(4);
+    should.not.exist(Project.id());
+
+
+    Project.resetAll();
+  });
+});
